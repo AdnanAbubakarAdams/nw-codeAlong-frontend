ee18de47e72cf5b736d588754a2781e39e8a79ae
AdnanAbubakarAdams <adnan_abubakaradams@yahoo.com>
2023-01-18 22:30:31 -0500

    components layout practice

---
+++ src/components/Container/Container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Container.css'
+
+const Container = ({ center, children }) => {
+    let classNames = ["Container"];
+    if (center) {
+        classNames.push("Container--center")
+    }
+    console.log(`<Container /> rendered!`)
+  return (
+    <div className={classNames.join(" ")}> 
+    {children}
+    </div>
+  )
+}
+
+export default Container;
